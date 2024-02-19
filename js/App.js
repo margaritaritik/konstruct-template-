7b4fa853ca918ffcbb9cb66b65ac77d91c516f56
margaritaritik <margaritakuryaeva@gmail.com>
2024-02-19 20:10:26 +0700

    scroll

--- js/App.js
+++ js/App.js
@@ -9,3 +9,19 @@ function myFunction() {
         x.className = "menu_list";
     }
 }
+document.addEventListener('click', e => {
+    // определяем, что клик произошёл на ссылке
+    if (e.target.nodeName === 'A' && e.target.) {
+        // отменяем переход по ссылке
+        e.preventDefault()
+        alert('Ой-ой! Здесь должна быть страница, но она ушла в отпуск на багамские острова. Мы уже высылаем ей билет обратно, так что скоро всё будет на своих местах!');
+    }
+})
+
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 500);
+}
